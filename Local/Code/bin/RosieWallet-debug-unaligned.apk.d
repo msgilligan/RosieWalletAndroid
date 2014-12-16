027033c4029be929bcc84b93cafa0ee78c73a2dd
hobnobber <hobnobber@users.noreply.github.com>
2014-12-16 17:37:54 -0500

    Delete RosieWallet-debug-unaligned.apk.d

--- Local/Code/bin/RosieWallet-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/mark/Rivetz/workspace/RivetzAndroidApps/CryptoCurrencies/Bitcoin/RosieWalletAndroid/Local/Code/bin/RosieWallet-debug-unaligned.apk : \
-/home/mark/Rivetz/workspace/RivetzAndroidApps/CryptoCurrencies/Bitcoin/RosieWalletAndroid/Local/Code/bin/RosieWallet.ap_ \
-/home/mark/Rivetz/workspace/RivetzAndroidApps/CryptoCurrencies/Bitcoin/RosieWalletAndroid/Local/Code/bin/classes.dex \
